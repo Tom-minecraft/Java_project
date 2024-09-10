c4f1a1d6eee92ac500fd8928f749105da3583b73
Tom <chqteng1234@163.com>
2024-09-10 11:45:29 +0800

    first_commit

---
+++ Pracs/src/Mywork/Thread_commute.java
@@ -0,0 +1,33 @@
+package Mywork;
+
+class print_num implements Runnable {
+    private int number = 1;
+
+    @Override
+    public void run() {
+       while(true){
+           synchronized (this) {
+               if (number <=100){
+                   try {
+                       Thread.sleep(10);
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                   }
+                   System.out.println(Thread.currentThread().getName()+":"+number++);
+               }else{
+                   break;
+               }
+           }
+       }
+    }
+}
+
+public class Thread_commute {
+    public static void main(String[] args) {
+        print_num p = new print_num();
+        Thread t1 = new Thread(p,"Thread-1");
+        Thread t2 = new Thread(p,"Thread-2");
+        t1.start();
+        t2.start();
+    }
+}
